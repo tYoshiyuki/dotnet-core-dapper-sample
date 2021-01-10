d2d5e1e40cded1cbc279ea2e306b0c325bd0442f
yTsunomoto <wasiwasimaru@gmail.com>
2021-01-11 01:19:31 +0900

    update 更新。

--- DotnetCoreDapperSample.Api/Messages.cs
+++ DotnetCoreDapperSample.Api/Messages.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DotnetCoreDapperSample.Api
+﻿namespace DotnetCoreDapperSample.Api
 {
     public static class Messages
     {
